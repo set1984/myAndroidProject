33b8e0dcded4ae4d4559afbcdb87b07a7d369aa2
Ming <Ming@Home>
2014-04-24 11:48:35 -0500

    test2

--- PracticeListView/src/com/example/practicelistview/MainActivity.java
+++ PracticeListView/src/com/example/practicelistview/MainActivity.java
@@ -8,6 +8,7 @@ import java.util.List;
 import android.content.Context;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.AdapterView;
@@ -21,6 +22,8 @@ public class MainActivity extends ActionBarActivity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 		
+		
+		Log.i("message", "haha");
 		
 	}
 	

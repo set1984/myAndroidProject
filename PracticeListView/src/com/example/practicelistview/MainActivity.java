60d053aa2410e13e8209e4f7b6fb59187e2587e1
Ming <Ming@Home>
2014-04-24 10:28:45 -0500

    Test

---
+++ PracticeListView/src/com/example/practicelistview/MainActivity.java
@@ -0,0 +1,38 @@
+//http://www.cnblogs.com/allin/archive/2010/05/11/1732200.html
+package com.example.practicelistview;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.ActionBarActivity;
+import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class MainActivity extends ActionBarActivity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+		
+		
+	}
+	
+	 
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main, menu);
+		return true;
+	}
+
+
+}
